2caf826905568329f7029522eec0afb466975c04
RiemannSum <owen.mcinnis@hotmail.com>
2021-06-22 16:03:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+console.log("asdasd")
+
+let e = _.filter(Game.spawns['Spawn1'].room.find(FIND_SOURCES));
+console.log(e.length)
+
+
 module.exports.loop = function(){
-	console.log("works")
+
 }
