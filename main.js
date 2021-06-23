6b10e06312b5b4c6b7f71fc91c8617db69271a3a
RiemannSum <owen.mcinnis@hotmail.com>
2021-06-23 09:07:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
 console.log("asdasd")
 
 let e = _.filter(Game.spawns['Spawn1'].room.find(FIND_SOURCES));
-console.log(e.length)
+// console.log(e.length)
 
-
+let findLocations = obj => {
+	let open = [];
+	if(obj.top){}
+}
 module.exports.loop = function(){
-
+	console.log(e[0].pos.x + " " + e[0].pos.y)
 }
