09398e6f7b00adc6352aa7b4c13ae9ff87bae5cc
RiemannSum <owen.mcinnis@hotmail.com>
2021-06-28 20:32:15 -0300

    Update prototype.creep.js

--- prototype.creep.js
+++ prototype.creep.js
@@ -4,4 +4,4 @@ var roles = {
 
 Creep.prototype.runRole = () => roles[this.memory.role].run(this);
 
-Creep.prototype.suicide = () = creep.say("RIP");
+Creep.prototype.suicide = () = creep.say("RIP"); //not sure if thisll work
